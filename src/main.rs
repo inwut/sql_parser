c32f6516c78311cc86e21a20f870b6d5794d847b
Daria Vetrykush <dariavetrykush@gmail.com>
2024-11-07 01:21:53 +0200

    create grammar, add lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use pest::Parser;
+use anyhow::anyhow;
+use sql_parser::*;
+
+fn main() -> anyhow::Result<()> {
+    let pair = Grammar::parse(Rule::group_by_clause, "GROUP BY name, surname HAVING SUM(n) >= 0")?.next().ok_or_else( || anyhow!( "no pair" ) )?;
+    dbg!(pair);
+    Ok(())
 }
